8d56056137a0c850300f3352d120d83a10b1fafe
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-05-31 15:58:42 -0400

    delete user feature

--- src/test/java/com/bookit/pages/BasePage.java
+++ src/test/java/com/bookit/pages/BasePage.java
@@ -2,6 +2,7 @@ package com.bookit.pages;
 
 import com.bookit.utilities.Driver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -19,13 +20,14 @@ public abstract class BasePage {
     protected WebElement self;
 
     protected WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 20);
+    protected Actions actions = new Actions(Driver.getDriver());
 
     public BasePage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
     public void goToSelfPage(){
-        wait.until(ExpectedConditions.elementToBeClickable(my)).click();
-        wait.until(ExpectedConditions.elementToBeClickable(self)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(my));
+        actions.moveToElement(my).pause(2000).click(self).perform();
     }
 }
