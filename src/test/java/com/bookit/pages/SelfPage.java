3cf2ae7b10d5b08c9f0ffb5a556b0561a0c9b637
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-05-31 16:19:46 -0400

    delete user feature

--- src/test/java/com/bookit/pages/SelfPage.java
+++ src/test/java/com/bookit/pages/SelfPage.java
@@ -1,5 +1,10 @@
 package com.bookit.pages;
 
+import com.bookit.utilities.BrowserUtilities;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
 public class SelfPage extends BasePage {
 
 
@@ -9,7 +14,9 @@ public class SelfPage extends BasePage {
      * @return it's value
      */
     public String getUserInfo(String value){
+        BrowserUtilities.waitForPageToLoad(10);
         String xpath = "//p[text()='"+value+"']/preceding-sibling::p";
-        return "";
+        WebElement valueElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
+        return valueElement.getText().trim();
     }
 }
